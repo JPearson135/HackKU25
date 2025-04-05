9b4490590ff841c0e014b52038a523cfe56600c1
thunyP <tpt8669@gmail.com>
2025-04-05 17:39:25 -0500

    organize dataset

--- kaggle/kaggle_data_organizer.py
+++ kaggle/kaggle_data_organizer.py
@@ -4,4 +4,17 @@ import pandas as pd
 
 kaggle.api.authenticate()  # Authenticate with Kaggle API
 
-kaggle.api.dataset_download_files('hanaksoy/health-and-sleep-statistics', path='.', unzip=True,)
+output_dir = "datasets"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)  # Create the directory if it doesn't exist
+
+kaggle.api.dataset_download_files(
+    'hanaksoy/health-and-sleep-statistics', 
+    path=output_dir, 
+    unzip=True)
+
+kaggle.api.dataset_download_files('shariful07/student-mental-health', 
+    path=output_dir,
+    unzip=True)
+
